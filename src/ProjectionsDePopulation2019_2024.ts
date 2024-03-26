438233b7e7d7bdf19d1508ab38e9b7a9a6f49946
BorisTherin <btherin@gmx.fr>
2024-03-26 20:22:30 +0100

    publish

--- src/ProjectionsDePopulation2019_2024.ts
+++ src/ProjectionsDePopulation2019_2024.ts
@@ -8,27 +8,30 @@ export class ProjectionsDePopulation2019_2024 {
   transformedFile: string = "./transformedData/proj_19np_transformed.csv"
 
 
-  constructor(remoteFile?: string) {
+  constructor(remoteFile: string, localRawFile: string, transformedFile: string) {
     if (remoteFile) this.localRawFile = remoteFile
+    this.localRawFile = localRawFile
+    this.remoteFile = remoteFile
+    this.transformedFile = transformedFile
+  }
+  run = async() => { 
+    await this.downLoad()
+    //await this.transform()
   }
 
   async downLoad(path?: string) {
     if (!path) path = this.localRawFile
     const res = await fetch(this.remoteFile)
+    const text = await res.text()
     const fileStream = fs.createWriteStream(path)
     await new Promise((resolve, reject) => {
-      //res.body?.pipe(fileStream)
-      //res.body?.on("error", reject)
-      //fileStream.write(res.body)
-      //fileStream.on("finish", resolve)
-      console.log(res.body)
+      fileStream.write(text)
     });
     this.localRawFile = path
   }
 
-  transform(destFile?: string) {
-    if (!destFile) destFile = this.transformedFile
-    const data: string = fs.readFileSync('./data/proj_19np.tsv', { encoding: 'utf8' })
+  transform() {
+    const data: string = fs.readFileSync(this.localRawFile, { encoding: 'utf8' })
     let df = pl.readCSV(data, { sep: ",", tryParseDates: true }).sample(1000)
     df = df.rename({"TIME_PERIOD":"time"}).rename({"OBS_FLAG":"population_proj"})
     df = df.select('time','DATAFLOW','LAST UPDATE','freq','unit','population_proj','sex','projection','age')
@@ -41,7 +44,8 @@ export class ProjectionsDePopulation2019_2024 {
         //pl.col("time").str.replace(pl.col("time"), new Date(pl.col("time")))
         pl.col('age').str.replace('Y_GE100','Y_OPEN') 
     )
-    df.writeCSV(destFile)
+    console.log(df.head(10))
+    df.writeCSV(this.transformedFile)
   }
 
   config(options: object) {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import {ProjectionsDePopulation2019_2024 as Projection} from "./ProjectionsDePopulation2019_2024"
 
-const proj = new Projection()
-proj.downLoad()
-//proj.transform()
+const proj = new Projection(
+  "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/proj_19np?format=TSV", 
+  "./rawData/proj_19np.csv",
+  "./transformedData/proj_19np_transformed.csv"
+)
+proj.run()
+

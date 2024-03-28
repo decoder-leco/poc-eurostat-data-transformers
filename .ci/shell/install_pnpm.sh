7daa7f6a0f25e303d9c66c2a82e70ca1f88ac9e1
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2024-03-28 19:10:11 +0100

    feat.(pipeline def.): fix pnpm installation

---
+++ .ci/shell/install_pnpm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo " [$0] -----------------------"
+echo " [$0] - BASH_ENV=[$BASH_ENV]"
+echo " [$0] -----------------------"
+
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+cat <<EOF >./add.on.bashrc
+export PNPM_HOME="/home/circleci/.local/share/pnpm"
+case ":\$PATH:" in
+*":\$PNPM_HOME:"*) ;;
+*) export PATH="\$PNPM_HOME:\$PATH" ;;
+esac
+EOF
+cat ./add.on.bashrc | tee -a "$BASH_ENV"

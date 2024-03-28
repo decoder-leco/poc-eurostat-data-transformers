58428c4dc34b6323dba16172c8d20c1074a8a21c
BorisTherin <btherin@gmx.fr>
2024-03-28 12:07:35 +0100

    described test

--- tests/projectionDePopulation2019-2024/Ingest.test.ts
+++ tests/projectionDePopulation2019-2024/Ingest.test.ts
@@ -1,11 +1,13 @@
 import * as projectionDePopulation from "../../src/projectionDePopulation2019-2024"
 
 const ingest = new projectionDePopulation.Ingest("./lol","./test")
+
 test('adds 1 + 2 to equal 3', () => {
   expect(ingest.addition(1, 2)).toBe(3);
 });
 
-/*
-const ingest = new projectionDePopulation.Ingest("./lol","./test")
-ingest.addition(2,3)
-*/
+describe("fonction d'addition", () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(ingest.addition(1, 2)).toBe(3);
+  });
+})

2ce74e20ed8004a179d09d8f357dfb3b69624aad
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2024-03-28 21:17:41 +0100

    fix missing exported pomme and abricot

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import * as projectionDePopulation from "./projectionDePopulation2019-2024"
-
+export * as abricot from './abricot/';
+export * as pomme from './pomme/';
 /*
 const ingest = new projectionDePopulation.Ingest(
     "proj_19np__custom_2224172_linear.csv",
     "./rawData/proj_19np.csv"
   )
 */
+
+/*
  const ingest = new projectionDePopulation.Ingest()
  ingest.init(
   "proj_19np__custom_2224172_linear.csv",
@@ -25,3 +28,4 @@ const run = async() => {
 run()
 
 
+*/

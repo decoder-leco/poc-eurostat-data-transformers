e5d12fb8d5c81d5f36cd439f401827c506ac71b7
BorisTherin <btherin@gmx.fr>
2024-03-29 12:00:39 +0100

    fonction back to classes

--- src/ingesters/handleDir.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from "node:fs"
-
-export async function createDirs(rawPath :string) {
-  if (fs.existsSync(rawPath.split("/")[1]) == false) {
-    fs.mkdirSync( rawPath.split("/")[1])
-    console.log("mkdir " + rawPath.split("/")[1] )
-  }
-}
--- src/ingesters/index.ts
+++ src/ingesters/index.ts
@@ -1,2 +1,1 @@
-export * from './Transform'
 export * from './DecoderLecoGithubDataIngester'

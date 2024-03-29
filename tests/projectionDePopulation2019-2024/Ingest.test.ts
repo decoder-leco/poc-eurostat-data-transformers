fb2c958a7e981f3de0c5add05a6eb7632c3de659
BorisTherin <btherin@gmx.fr>
2024-03-29 11:10:34 +0100

    fonction back to classes

--- tests/projectionDePopulation2019-2024/Ingest.test.ts
+++ tests/projectionDePopulation2019-2024/Ingest.test.ts
@@ -2,6 +2,7 @@ import * as projection from "../../src/projectionDePopulation2019-2024"
 import { handleDirs } from "../../src/projectionDePopulation2019-2024/handleDir"
 import { download } from "../../src/projectionDePopulation2019-2024/download"
 
+/*
 jest.mock('../../src/projectionDePopulation2019-2024/handleDir', () => ({
   handleDirs: jest.fn(),
 }))
@@ -9,6 +10,7 @@ jest.mock('../../src/projectionDePopulation2019-2024/handleDir', () => ({
 jest.mock('../../src/projectionDePopulation2019-2024/download', () => ({
   download: jest.fn(),
 }))
+*/
 
 const ingest = new projection.Ingest("test", "./tmp/test.cvs")
 
@@ -27,7 +29,7 @@ describe('Testing - projectionDePopulation2019-2024 Ingestion', () => {
 
       // TEST PASS WITH toHaveBeenCalledTimes(1)
       // TEST FAIL WITH toHaveBeenCalledTimes(2)
-      expect(handleDirs).toHaveBeenCalledTimes(1)
+      expect(ingest.handleDir()).toHaveBeenCalledTimes(1)
     })
   })
 
@@ -37,7 +39,7 @@ describe('Testing - projectionDePopulation2019-2024 Ingestion', () => {
 
       // TEST PASS WITH toHaveBeenCalledTimes(2)
       // TEST FAIL WITH toHaveBeenCalledTimes(1)
-      expect(download).toHaveBeenCalledTimes(2)
+      expect(ingest.download()).toHaveBeenCalledTimes(2)
     })
   })
 })
